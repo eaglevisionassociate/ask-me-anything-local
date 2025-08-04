cc8a440810a2772f737c6e183a6f4a3cb2628c90
eaglevisionassociate <eaglevisonassociate@gmail.com>
2025-08-04 14:18:00 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
-//import { componentTagger } from "react";
+//import { componentTagger } from "lovable-tagger";
+import { componentTagger } from "react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -12,8 +12,10 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
+    //mode === 'development' &&
+    //componentTagger(),
     mode === 'development' &&
-    componentTagger(),
+    react(),
   ].filter(Boolean),
   resolve: {
     alias: {
